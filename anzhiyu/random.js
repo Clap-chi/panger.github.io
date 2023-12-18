7e2a459a1851d973fb8a5da171cf59811114b936
wzy <wzy2096@126.com>
2023-12-18 23:03:34 +0800

    Site updated: 2023-12-18 23:03:34

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["blogs/713c1ac0.html","blogs/1155b381.html","blogs/66f88466.html","blogs/81b624c3.html","blogs/b9528813.html","blogs/3006dc10.html","blogs/2d14aaf0.html","blogs/2b7e9eb2.html","blogs/6fbd99c.html","blogs/22e916fa.html"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
