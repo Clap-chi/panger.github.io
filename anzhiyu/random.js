c21b2c810d9da917039a78315e9eca1b3cca0a6d
wzy <wzy2096@126.com>
2023-12-19 19:23:33 +0800

    Site updated: 2023-12-19 19:23:26

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["blogs/6fbd99c.html","blogs/2d14aaf0.html","blogs/b9528813.html","blogs/66f88466.html","blogs/81b624c3.html","blogs/713c1ac0.html","blogs/1155b381.html","blogs/3006dc10.html","blogs/22e916fa.html","blogs/2b7e9eb2.html"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
